0355f705bd640f60a90964eea7feba8c6e1e8bf0
Matthew Martin <matthew-martin@programmer.net>
2022-11-01 19:44:46 -0400

    Fixed the footer

--- bankui/src/components/static/Footer.js
+++ bankui/src/components/static/Footer.js
@@ -2,10 +2,10 @@ export default function Footer() {
     const currentYear = new Date().getFullYear();
 
     return(
-        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top fixed-bottom">
-            <div className="col-md-12 d-flex text-center">
+        <footer className="d-flex flex-wrap py-4 mt-4 border-top fixed-bottom bg-dark">
+            <div className="col-md-12 d-flex text-center footer">
                 <div className="col-md-4"/>
-                <span className="mb-3 mb-md-0 text-danger col-md-4">© {currentYear} Horrorbank, Inc</span>
+                <span className="mb-3 mb-md-0 text-danger col-md-4">© {currentYear} Horrorbank. Inc</span>
                 <div className="col-md-4"/>
             </div>
         </footer>
